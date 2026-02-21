98c6b3f7ceb0aafa9786f366b8cef4facf540890
twisti <denizgebauer@web.de>
2026-02-21 22:33:08 +0100

    Add logging for Redis queue fetching: replace print statements with structured logging for improved traceability.

--- surf-queue-common/src/main/kotlin/dev/slne/surf/queue/common/queue/RedisQueueService.kt
+++ surf-queue-common/src/main/kotlin/dev/slne/surf/queue/common/queue/RedisQueueService.kt
@@ -7,6 +7,7 @@ import dev.slne.surf.queue.api.service.SurfQueueService
 import dev.slne.surf.queue.common.SurfQueueInstance
 import dev.slne.surf.queue.common.redis.redisApi
 import dev.slne.surf.redis.libs.redisson.api.options.KeysScanOptions
+import dev.slne.surf.surfapi.core.api.util.logger
 import kotlinx.coroutines.reactive.asFlow
 
 @OptIn(InternalSurfQueueApi::class)
@@ -30,15 +31,15 @@ class RedisQueueService : SurfQueueService {
                 val serverName =
                     it.replaceFirst(RedisQueueKeys.QUEUE_PREFIX, "").replaceFirst(RedisQueueKeys.EPOCH_MS_SUFFIX, "")
 
-                repeat(20) {
-                    println("Found queue for server $serverName in Redis, fetching...")
-                }
+                log.atInfo()
+                    .log("Found queue for server $serverName in Redis, fetching...")
 
                 get(serverName)
             }
     }
 
     companion object {
+        private val log = logger()
         fun get() = SurfQueueService.instance as RedisQueueService
     }
 }
